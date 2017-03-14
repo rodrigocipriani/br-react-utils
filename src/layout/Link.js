42dd068481f143d6a43f637265b66501dfab8d6c
Rodrigo Cipriani da Rosa <rcipriani@bb.com.br>
2017-03-14 16:01:07 -0300

    Incluído indexS .js

--- src/layout/Link.js
+++ src/layout/Link.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {Component, PropTypes} from "react";
 
-const Link = ({...props, children, href}) => (
+class Link extends Component {
 
-    <a {...props} href={(href ? href : 'javascript:void(0)')}>{children}</a>
-);
+    render() {
+
+        const {children, href} = this.props;
+
+        return (
+            <a {...this.props} href={(href ? href : 'javascript:void(0)')}>{children}</a>
+        );
+    }
+}
+
+
+Link.propTypes = {
+    href: React.PropTypes.string,
+    children: React.PropTypes.any
+};
+
+Link.defaultProps = {
+    href: null,
+    children: null
+};
 
 export default Link;

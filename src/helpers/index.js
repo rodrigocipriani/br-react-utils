0a74500149c8ad50b06c26d3d1d35be0e246ec69
rodrigocipriani <chopsss@gmail.com>
2017-04-19 21:25:34 -0300

    alterado helper api

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,4 @@
-export {default as api} from './api';
+export api from './api';
 export {default as mes} from './mes';
 export {default as DataHelper} from './DataHelper';
 export {default as MoedaHelper} from './MoedaHelper';

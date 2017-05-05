c0646704627a6d7871105b667942dbc18c283464
Rodrigo Cipriani da Rosa <rcipriani@bb.com.br>
2017-05-05 16:18:29 -0300

    Component Paginator

---
+++ src/layout/Paginator.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+
+class Paginator extends Component {
+
+    _renderPaginator = (totalPages, currentPage) => {
+        let ret = [];
+        for (let i = 1; i <= totalPages; i++) {
+            const active = currentPage === i ? "active" : "";
+            ret.push(<li key={i} onClick={() => this._handleChangePage(i)}
+                         className={['waves-effect', active].join(' ')}><a href="#!">{i}</a>
+            </li>);
+        }
+        return ret;
+    };
+
+    _handleChangePage = (pageNumber) => {
+        this.props.onChangePage(pageNumber);
+    };
+
+    _nextPage = (currentPage, isFirstPage) => {
+        if (!isFirstPage)
+            this._handleChangePage(currentPage + 1);
+    };
+
+    _prevPage = (currentPage, isLastPage) => {
+        if (!isLastPage)
+            this._handleChangePage(currentPage - 1);
+    };
+
+    render() {
+
+        const {totalPages, currentPage} = this.props;
+
+        const isFirstPage = currentPage === 1;
+        const isLastPage = currentPage === totalPages;
+
+        return (
+            <ul className="pagination center-align">
+                <li onClick={() => this._prevPage(currentPage, isFirstPage)}
+                    className={["", (isFirstPage ? 'disabled' : 'waves-effect')].join(' ')}>
+                    <a href="#!"><i className="fa fa-chevron-left" aria-hidden="true"/></a>
+                </li>
+                {this._renderPaginator(totalPages, currentPage)}
+                <li onClick={() => this._nextPage(currentPage, isLastPage)}
+                    className={["", (isLastPage ? 'disabled' : 'waves-effect')].join(' ')}>
+                    <a href="#!"><i className="fa fa-chevron-right" aria-hidden="true"/></a>
+                </li>
+            </ul>
+        );
+    }
+}
+
+Paginator.propTypes = {
+    totalPages: React.PropTypes.number.isRequired,
+    currentPage: React.PropTypes.number.isRequired,
+    onChangePage: React.PropTypes.func.isRequired
+};
+
+Paginator.defaultProps = {
+    totalPages: 0,
+    currentPage: 1,
+    onChangePage: null
+};
+
+export default Paginator;

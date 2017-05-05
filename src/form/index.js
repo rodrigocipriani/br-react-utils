c25aa505f9f491f09f0f94aaa2a69bc850930651
Rodrigo Cipriani da Rosa <rcipriani@bb.com.br>
2017-05-05 12:58:04 -0300

    New forms components

--- src/form/index.js
+++ src/form/index.js
@@ -1,6 +1,2 @@
-export Form from 'muicss/lib/react/form';
-export Input from 'muicss/lib/react/input';
-export Button from 'muicss/lib/react/button';
-
 export {default as TextField} from './TextField';
 export {default as SelectFieldInput} from './SelectField';

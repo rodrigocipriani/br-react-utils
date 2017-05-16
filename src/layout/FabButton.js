1da5ef912996f95e8d95caf1f42ab2090f7d7f52
rodrigocipriani <chopsss@gmail.com>
2017-05-16 20:59:51 -0300

    PropTypes via the main React package is deprecated. Use the prop-types package from npm instead

--- src/layout/FabButton.js
+++ src/layout/FabButton.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import 'materialize-css/bin/materialize.css';
 import 'materialize-css/bin/materialize.js';
 

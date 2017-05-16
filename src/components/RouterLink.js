ddf6caf24bc53bc5473656edd5650821c75e782c
rodrigocipriani <chopsss@gmail.com>
2017-05-15 21:59:49 -0300

    retirado dependencia react-router

--- src/components/RouterLink.js
+++ src/components/RouterLink.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router';
+// import {Link} from 'react-router';
 
 const RouterLink = ({...props, children, href}) => (
 
-    <Link activeClassName='light-blue-text text-darken-4 grey lighten-4'  className="waves-effect" {...props}>
-        {children}
-    </Link>
+    {/*<Link activeClassName='light-blue-text text-darken-4 grey lighten-4'  className="waves-effect" {...props}>*/}
+        {/*{children}*/}
+    {/*</Link>*/}
+    <div>link corrigir</div>
 );
 
 const css = {

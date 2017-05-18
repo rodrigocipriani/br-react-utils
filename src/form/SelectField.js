50f1a0737f14fbb29a82b674669481e99bf9b832
rodrigocipriani <chopsss@gmail.com>
2017-05-17 21:32:38 -0300

    PropTypes via the main React package is deprecated. Use the prop-types package from npm instead

--- src/form/SelectField.js
+++ src/form/SelectField.js
@@ -1,44 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-class SelectFieldInput extends Component{
+class SelectFieldInput extends Component {
 
-    static propTypes = {
-        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-        label: PropTypes.string,
-        data: PropTypes.arrayOf(PropTypes.shape({
-            value: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-        }))
-    };
+  static propTypes = {
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    label: PropTypes.string,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })),
+  };
 
-    static defaultProps = {
-        sizes: ''
-    };
+  static defaultProps = {
+    sizes: '',
+  };
 
-    render(){
+  render() {
+    const { value, label, data } = this.props;
 
-        const {value, label, data} = this.props;
-
-        return (
-            <SelectField {...this.props} floatingLabelStyle={style.label} floatingLabelText={label} value={value} >
-                {data && data.map((item, key) => (
-                    <MenuItem {...item} value={item.value} primaryText={item.text} key={key}  />
+    return (
+      <SelectField { ...this.props } floatingLabelStyle={ style.label } floatingLabelText={ label } value={ value } >
+        {data && data.map((item, key) => (
+          <MenuItem { ...item } value={ item.value } primaryText={ item.text } key={ key } />
                 ))}
-            </SelectField>
+      </SelectField>
 
-        );
-    }
+    );
+  }
 }
 
 const style = {
-    label: {
-        fontSize: '1.5em',
-        color: '#666',
-        left: 0
-    }
+  label: {
+    fontSize: '1.5em',
+    color: '#666',
+    left: 0,
+  },
 };
 
 export default SelectFieldInput;

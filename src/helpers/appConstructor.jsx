387df793012a72ea40df8bfb8c82041f13adcc99
rodrigocipriani <chopsss@gmail.com>
2017-05-17 23:58:23 -0300

    Melhora app constructor

--- src/helpers/appConstructor.jsx
+++ src/helpers/appConstructor.jsx
@@ -4,19 +4,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+    withRouter
+} from 'react-router-dom';
 import 'babel-polyfill';
+
+const customHistory = createBrowserHistory();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default (Routes, store, rootContainer = 'root') => {
     ReactDOM.render(
         <Provider store={ store }>
-            { isProduction ?
-                <Routes /> :
-                <div>
-                    <Routes />
-                    {/* <div>Desenvolvimento</div>*/}
-                </div> }
+            <Router history={ customHistory }>
+                { isProduction ?
+                    <Routes /> :
+                    <div>
+                        <Routes />
+                        {/* <div>Desenvolvimento</div>*/}
+                    </div> }
+            </Router>
         </Provider>,
         document.getElementById(rootContainer)
     );

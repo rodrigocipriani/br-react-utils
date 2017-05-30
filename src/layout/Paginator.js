6b6c399daac2c3cbe2694c9286acf0a51fec6b41
Rodrigo Cipriani da Rosa <rcipriani@bb.com.br>
2017-05-30 09:58:26 -0300

    Melhorias

--- src/layout/Paginator.js
+++ src/layout/Paginator.js
@@ -1,66 +1,73 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+console.error('DEPRECATED (Paginator.js) - Usar o br-react-utils. [Observar versão do router]');
 
 class Paginator extends Component {
 
-    _renderPaginator = (totalPages, currentPage) => {
-        let ret = [];
-        for (let i = 1; i <= totalPages; i++) {
-            const active = currentPage === i ? "active" : "";
-            ret.push(<li key={i} onClick={() => this._handleChangePage(i)}
-                         className={['waves-effect', active].join(' ')}><a href="#!">{i}</a>
-            </li>);
-        }
-        return ret;
-    };
+  handleChangePage = (pageNumber) => {
+    this.props.onChangePage(pageNumber);
+  };
 
-    _handleChangePage = (pageNumber) => {
-        this.props.onChangePage(pageNumber);
-    };
+  nextPage = (currentPage, isFirstPage) => {
+    if (!isFirstPage) { this.handleChangePage(currentPage + 1); }
+  };
 
-    _nextPage = (currentPage, isFirstPage) => {
-        if (!isFirstPage)
-            this._handleChangePage(currentPage + 1);
-    };
+  prevPage = (currentPage, isLastPage) => {
+    if (!isLastPage) { this.handleChangePage(currentPage - 1); }
+  };
 
-    _prevPage = (currentPage, isLastPage) => {
-        if (!isLastPage)
-            this._handleChangePage(currentPage - 1);
-    };
-
-    render() {
-
-        const {totalPages, currentPage} = this.props;
-
-        const isFirstPage = currentPage === 1;
-        const isLastPage = currentPage === totalPages;
-
-        return (
-            <ul className="pagination center-align">
-                <li onClick={() => this._prevPage(currentPage, isFirstPage)}
-                    className={["", (isFirstPage ? 'disabled' : 'waves-effect')].join(' ')}>
-                    <a href="#!"><i className="fa fa-chevron-left" aria-hidden="true"/></a>
-                </li>
-                {this._renderPaginator(totalPages, currentPage)}
-                <li onClick={() => this._nextPage(currentPage, isLastPage)}
-                    className={["", (isLastPage ? 'disabled' : 'waves-effect')].join(' ')}>
-                    <a href="#!"><i className="fa fa-chevron-right" aria-hidden="true"/></a>
-                </li>
-            </ul>
-        );
+  renderPaginator = (totalPages, currentPage) => {
+    const ret = [];
+    for (let i = 1; i <= totalPages; i++) {
+      const active = currentPage === i ? 'active' : '';
+      ret.push(
+          <li
+              key={ i } onClick={ () => this.handleChangePage(i) }
+              className={ ['waves-effect', active].join(' ') }
+          >
+            <a href='#!'>{i}</a>
+          </li>
+      );
     }
+    return ret;
+  };
+
+  render() {
+    const { totalPages, currentPage } = this.props;
+
+    const isFirstPage = currentPage === 1;
+    const isLastPage = currentPage === totalPages;
+
+    return (
+        <ul className='pagination center-align'>
+          <li
+              onClick={ () => this.prevPage(currentPage, isFirstPage) }
+              className={ ['', (isFirstPage ? 'disabled' : 'waves-effect')].join(' ') }
+          >
+            <a href='#!'><i className='fa fa-chevron-left' aria-hidden='true'/></a>
+          </li>
+          {this.renderPaginator(totalPages, currentPage)}
+          <li
+              onClick={ () => this.nextPage(currentPage, isLastPage) }
+              className={ ['', (isLastPage ? 'disabled' : 'waves-effect')].join(' ') }
+          >
+            <a href='#!'><i className='fa fa-chevron-right' aria-hidden='true'/></a>
+          </li>
+        </ul>
+    );
+  }
 }
 
 Paginator.propTypes = {
-    totalPages: PropTypes.number.isRequired,
-    currentPage: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired
+  totalPages  : React.PropTypes.number.isRequired,
+  currentPage : React.PropTypes.number.isRequired,
+  onChangePage: React.PropTypes.func.isRequired
 };
 
 Paginator.defaultProps = {
-    totalPages: 0,
-    currentPage: 1,
-    onChangePage: null
+  totalPages  : 0,
+  currentPage : 1,
+  onChangePage: null
 };
 
 export default Paginator;
--- src/layout/SideNav.js
+++ src/layout/SideNav.js
@@ -1,100 +1,101 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, PropTypes } from 'react';
 
-
+const paddingLeft = 300;
+const maxWidth = 1200 + paddingLeft;
 const styles = {
-    isFixed: {
-        paddingLeft: 300
-    },
-    sideMenuTitle: {
-        paddingLeft: '10px',
-        color: '#333',
-        textShadow: 'rgb(224, 224, 224) 1px 1px 0px'
-    }
+  isFixed: {
+    paddingLeft,
+    maxWidth
+  },
+  sideMenuTitle: {
+    paddingLeft: '10px',
+    color      : '#333',
+    textShadow : 'rgb(224, 224, 224) 1px 1px 0px'
+  },
+  SideNav: {
+    maxWidth
+  }
 };
 
 class SideNav extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+  // componentDidUpdate() {
+  //     $('.button-collapse').sideNav();
+  // }
+  //
+  // componentDidMount() {
+  //     $(document).ready(() => {
+  //         $('.button-collapse').sideNav();
+  //     });
+  // }
 
-    // componentWillReceiveProps(nextProps){
-    //     console.log("forceRecise");
-    //     window.dispatchEvent(new Event('resize'));
-    // }
+  render() {
+    const { sideMenu, sideMenuTitle, sideMenuHeaderBg, avatarImg, avatarTitle, avatarSubTitle,
+            isFixed, isMenuOpen, children, sideMenuFooter } = this.props;
 
-    componentDidUpdate() {
-        $(".button-collapse").sideNav();
-    }
+    const isShowMenu = isMenuOpen && isFixed;
 
-    componentDidMount() {
-        $(document).ready(function () {
-            $(".button-collapse").sideNav();
-        });
-    }
+    return (
+        <div
+            className={ ['SideNav', (isShowMenu ? 'container' : '')].join(' ') }
+            style={ Object.assign(isShowMenu ? styles.isFixed : {}, {}) }
+        >
+          {/* <a href="#"
+           data-activates="slide-out"
+           className="button-collapse
+           waves-effect waves-light btn">Menu</a>*/}
+          {children}
 
-    render() {
-        const {sideMenu, sideMenuTitle, sideMenuHeaderBg, avatarImg, avatarTitle, avatarSubTitle, isFixed, isMenuOpen, children, sideMenuFooter} = this.props;
+            <ul id='slide-out' className={ ['side-nav', (isShowMenu ? 'fixed' : '')].join(' ') }>
+                <li>
+                    <div className='userView'>
+                        <img className='background background-cover' src={ sideMenuHeaderBg }/>
+                        <a href='#!user'>
+                            <img className='circle' src={ avatarImg }/>
+                        </a>
+                        <a href='#!name'><span className='white-text name'>{avatarTitle}</span></a>
+                        <a href='#!email'><span className='white-text email'>{avatarSubTitle}</span></a>
+                    </div>
+                    <div style={ { position: 'absolute', bottom: 50, width: '100%' } }>
+                        <div style={ { width: '100%', textAlign: 'right' } }>{sideMenuFooter}</div>
+                    </div>
+                </li>
+                <li className='white'><a style={ { fontSize: '1.5em' } }>{sideMenuTitle}</a></li>
+                <li>
+                  {sideMenu}
+                </li>
+            </ul>
 
-        const isShowMenu = isMenuOpen && isFixed;
-
-        return (
-            <div className={['SideNav', (isShowMenu ? 'container' : '')].join(' ')}
-                 style={isShowMenu ? styles.isFixed : {}}>
-                {/*<a href="#" data-activates="slide-out" className="button-collapse waves-effect waves-light btn">Menu</a>*/}
-                {children}
-
-                <ul id="slide-out" className={['side-nav', (isShowMenu ? 'fixed' : '')].join(' ')}>
-                    <li>
-                        <div className="userView">
-                            <img className="background background-cover" src={sideMenuHeaderBg}/>
-                            <a href="#!user">
-                                <img className="circle" src={avatarImg}/>
-                            </a>
-                            <a href="#!name"><span className="white-text name">{avatarTitle}</span></a>
-                            <a href="#!email"><span className="white-text email">{avatarSubTitle}</span></a>
-                        </div>
-                        <div style={{position: 'absolute', bottom: 50, width: '100%'}}>
-                            <div style={{width: '100%', textAlign: 'right'}}>{sideMenuFooter}</div>
-                        </div>
-                    </li>
-                    <li className="white"><a style={{fontSize: '1.5em'}}>{sideMenuTitle}</a></li>
-                    <li>
-                        {sideMenu}
-                    </li>
-                </ul>
-
-            </div>
-        );
-    }
+        </div>
+    );
+  }
 }
 
 SideNav.propTypes = {
-    children: PropTypes.any,
-    sideMenu: PropTypes.any,
-    sideMenuTitle: PropTypes.any,
-    sideMenuFooter: PropTypes.any,
-    sideMenuHeaderBg: PropTypes.string,
-    avatarImg: PropTypes.string,
-    avatarTitle: PropTypes.string,
-    avatarSubTitle: PropTypes.string,
-    isMenuOpen: PropTypes.bool,
-    isFixed: PropTypes.bool
+  children        : PropTypes.any,
+  sideMenu        : PropTypes.any,
+  sideMenuTitle   : PropTypes.any,
+  sideMenuFooter  : PropTypes.any,
+  sideMenuHeaderBg: PropTypes.string,
+  avatarImg       : PropTypes.string,
+  avatarTitle     : PropTypes.string,
+  avatarSubTitle  : PropTypes.string,
+  isMenuOpen      : PropTypes.bool,
+  isFixed         : PropTypes.bool
 };
 
 SideNav.defaultProps = {
-    modoApresentacao: false,
-    children: <div>Carregando...</div>,
-    sideMenuTitle: '',
-    sideMenuFooter: null,
-    sideMenu: null,
-    sideMenuHeaderBg: '',
-    avatarImg: '',
-    avatarTitle: '',
-    avatarSubTitle: '',
-    isMenuOpen: true,
-    isFixed: true
+  modoApresentacao: false,
+  children        : <div>Carregando...</div>,
+  sideMenuTitle   : '',
+  sideMenuFooter  : null,
+  sideMenu        : null,
+  sideMenuHeaderBg: '',
+  avatarImg       : '',
+  avatarTitle     : '',
+  avatarSubTitle  : '',
+  isMenuOpen      : true,
+  isFixed         : true
 };
 
 export default SideNav;
